47ca30df672bb330e03971e504607feb58cfc702
zhong mingyuan <mralenzhong@qq.com>
2022-05-19 10:54:04 +0800

    增加hucky

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,11 @@
  * @Author: MrAlenZhong
  * @Date: 2022-05-16 15:37:28
  * @LastEditors: MrAlenZhong
- * @LastEditTime: 2022-05-16 18:19:24
+ * @LastEditTime: 2022-05-19 10:53:53
  * @Description:
  */
 import { createRouter, createWebHashHistory } from "vue-router";
-import { basicRoutes } from "./routes";
-
-console.log("basicRoutes  ", basicRoutes);
+import { basicRoutes } from "./routes";console.log("basicRoutes  ", basicRoutes);
 
 export const router = createRouter({
   history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
